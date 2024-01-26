e2fa285792387fec92b7aeb80b79e4e4e11493f7
riyajul505 <rlriyajul@gmail.com>
2024-01-27 00:49:37 +0600

    file adding

---
+++ Milestone 4 Javascript/day25.js
@@ -0,0 +1,26 @@
+// Variable
+
+var weight = 38; 
+var price = 38; 
+var year = 1995;  
+pok = 20;
+// var age; undefined variable
+year = 2001;
+// console.log(pok)
+
+var string = 'sadaruddin';
+// console.log(string)
+
+var rich = true; 
+// console.log(typeof rich)
+
+var string_to_int = parseInt('40')
+var price = 30;
+// console.log(typeof (string_to_int + price));
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(total.toFixed(3))
+var a = isNaN(2-10)
+console.log(a);
---
+++ Milestone 4 Javascript/day26.js
@@ -0,0 +1,57 @@
+// Comparison
+// and comparison && , or comparison ||
+//  === checks value and type also (10 === '10')
+
+// if (3 == 5) {
+//     console.log('true')
+// }
+// else {
+//     console.log('mama')
+// }
+
+// const salary = 2000;
+// const isBCS = true;
+
+// if (salary > 20000 && isBCS == true){
+//     console.log('mama paisi')
+// }
+// else{
+//     console.log('painai')
+// }
+
+// if ( 3 > 5 ){
+
+// }
+// else if ( 5 == 5){
+//     console.log('lol')
+// }
+// else{
+//     console.log('nai')
+// }
+
+// Ternary ____ Ternary ___ Shorthand
+
+// const age = 2;
+
+// age >= 18 ? console.log("Vote dao") : console.log('Ghumay thako')
+// let price = 100;
+// const isLeader = false;
+
+// if (isLeader === true){
+//     price = 0;
+// }
+// else{
+//     price += 100
+// }
+// console.log(price)
+
+// price = isLeader === true ? 0 : price += 100;
+// console.log(price)
+
+// const isPassed = false;
+
+// if (!isPassed){
+//     console.log('mair dibo')
+// }
+
+// console.log (6 != 6)
