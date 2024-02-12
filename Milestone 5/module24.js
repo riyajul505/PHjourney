26a5c9356b62be8b1d37e58fe85cf1da51b00b98
riyajul505 <rlriyajul@gmail.com>
2024-02-13 00:23:20 +0600

    adding new files

---
+++ Milestone 5/module24.js
@@ -0,0 +1,32 @@
+// const liset = document.getElementsByTagName('li');
+
+// console.log(liset)
+
+// for (const li of liset){
+//     console.log(li.innerText)
+// }
+
+// const fruitTitle = document.getElementById("fruit-tittle")
+
+// const fruitNames = document.getElementById('fruit-tittle')
+// console.log(fruitNames)
+
+
+// const sections = document.querySelectorAll('section');
+
+// for (const section of sections){
+//     section.style.border = '1px solid white';
+//     section.style.marginBottom = '5px';
+//     section.style.backgroundColor = 'lightgrey';
+// }
+
+
+// const places = document.getElementById('places-list');
+
+// const li = document.createElement('li');
+// li.innerText = 'this is a list';
+
+// places.appendChild(li);
+
+
+
