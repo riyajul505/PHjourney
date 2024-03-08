faaa0061f74b9fddb2af4198bfae294264fd33e1
riyajul505 <rlriyajul@gmail.com>
2024-03-09 01:41:24 +0600

    react file added

---
+++ my-first-react/src/ToDo.jsx
@@ -0,0 +1,13 @@
+// export default function Todo({task,isDone}){
+//     return (
+//         // <li>{isDone ? 'Finished' : 'Work on' } : {task}</li>
+//         <li>{task} {isDone || ': Do it'}</li>
+//     )
+// }
+
+export default function Actors({name}){
+    return (
+        <li>Name: {name}</li>
+    )
+}
+
