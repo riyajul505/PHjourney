e46f5cf32b60704365b0f7a924c0214e20b1ce92
riyajul505 <rlriyajul@gmail.com>
2024-03-18 19:08:39 +0600

    new module

--- React/react-core-2/src/count.jsx
+++ React/react-core-2/src/count.jsx
@@ -1,11 +1,11 @@
 import './Box.css';
-export default function Counter(player){
+export default function Counter({player}){
     // const [num, setNum] = useState(1);
     // const handleAdd = () => {
     //     const newNum = num+1;
     //     setNum(newNum);
     // }
-    const {name} = player.player;
+    const {name} = player;
     return(
         <>
             <h3 className='box'>Player Name: {name}</h3>
