0149950a8f9787ba3cced63d5157473320130b74
riyajul505 <rlriyajul@gmail.com>
2024-03-18 19:08:39 +0600

    new module

---
+++ React/test-daisy-ui/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className=''>Vite + React</h1>
+      <h1 className='text-7xl text-center text-red-600'>LOL</h1>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+
+    </>
+  )
+}
+
+export default App
