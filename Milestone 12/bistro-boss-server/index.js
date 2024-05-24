7bdeaeca7ac9eff37f032d3328d7b80fe94df1e9
riyajul505 <rlriyajul@gmail.com>
2024-05-24 22:57:24 +0600

    user data adding after login or register

--- Milestone 12/bistro-boss-server/index.js
+++ Milestone 12/bistro-boss-server/index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixzkh9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,39 +25,53 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("Bistro-Boss").collection("users");
     const menuCollection = client.db("Bistro-Boss").collection("menus");
     const reviewCollection = client.db("Bistro-Boss").collection("reviews");
     const cartCollection = client.db("Bistro-Boss").collection("cart");
+    // user api
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      const checkExist = await usersCollection.findOne({ email: data.email });
+      if (checkExist) {
+        return res.send({ message: "user already exist" });
+      }
 
-    app.get('/menus', async(req, res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    })
-    app.get('/reviews', async(req, res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-    })
+      const result = await usersCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/menus", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
     //cart collection
-    app.post('/cart', async(req, res)=>{
+    app.post("/cart", async (req, res) => {
       const data = req.body;
       const result = await cartCollection.insertOne(data);
       res.send(result);
-    })
-    app.get('/cart', async(req, res)=>{
+    });
+    app.get("/cart", async (req, res) => {
       const email = req.query.email;
-      const result = await cartCollection.find({email:email}).toArray();
+      const result = await cartCollection.find({ email: email }).toArray();
       res.send(result);
-    })
-    app.delete('/cart/:id', async(req, res)=>{
+    });
+    app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -68,12 +79,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Boss is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Boss is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
