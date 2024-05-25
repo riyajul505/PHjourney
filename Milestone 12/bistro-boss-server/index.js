296195f79b9aea26c6ec8e8a5f141874ca57510b
riyajul505 <rlriyajul@gmail.com>
2024-05-25 10:46:49 +0600

    user add,delete,update role, everyting working properly

--- Milestone 12/bistro-boss-server/index.js
+++ Milestone 12/bistro-boss-server/index.js
@@ -30,6 +30,27 @@ async function run() {
     const reviewCollection = client.db("Bistro-Boss").collection("reviews");
     const cartCollection = client.db("Bistro-Boss").collection("cart");
     // user api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch("/users/makeAdmin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "Admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const data = req.body;
       const checkExist = await usersCollection.findOne({ email: data.email });
@@ -41,6 +62,7 @@ async function run() {
       res.send(result);
     });
 
+    //menu related api
     app.get("/menus", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
