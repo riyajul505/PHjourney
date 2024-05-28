fbbd69757c9efe8cbf44d7da74dba4fb31ee220d
riyajul505 <rlriyajul@gmail.com>
2024-05-28 21:15:34 +0600

    admin can delete items from menu

--- Milestone 12/bistro-boss-server/index.js
+++ Milestone 12/bistro-boss-server/index.js
@@ -55,53 +55,62 @@ async function run() {
       });
     };
     // verify Admin
-    const verifyAdmin = async (req, res, next) =>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email};
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      const isAdmin = user?.role == 'Admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'forbidden access'})
-      }
-      next();
-    }
-    // check if this email is admin or not
-    app.get("/users/admin/:email", verifyToken, verifyAdmin, async (req, res) => {
-      const email = req.params.email;
-      if (email !== req.decoded.email) {
+      const isAdmin = user?.role == "Admin";
+      if (!isAdmin) {
         return res.status(403).send({ message: "forbidden access" });
       }
-      const query = { email: email };
-      const result = await usersCollection.findOne(query);
-      let admin = false;
-      if (result) {
-        admin = user?.role === "Admin";
+      next();
+    };
+    // check if this email is admin or not
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        const query = { email: email };
+        const result = await usersCollection.findOne(query);
+        let admin = false;
+        if (result) {
+          admin = result?.role === "Admin";
+        }
+        res.send({ admin });
       }
-      res.send({ admin });
-    });
+    );
     // user api
     app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
-      console.log(req.headers);
+      // console.log(req.headers);
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
-    app.delete("/users/:id",verifyToken, verifyAdmin, async (req, res) => {
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
-    app.patch("/users/makeAdmin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          role: "Admin",
-        },
-      };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/users/makeAdmin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "Admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
     app.post("/users", async (req, res) => {
       const data = req.body;
       const checkExist = await usersCollection.findOne({ email: data.email });
@@ -118,6 +127,18 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: id};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result)
+    })
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res)=>{
+      const data = req.body;
+      const result = await menuCollection.insertOne(data);
+      res.send(result);
+    })
+    // reviews api
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
